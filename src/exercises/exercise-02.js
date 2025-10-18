b4d6fc6e8dfec450be3413365b9a56bb2b0a191a
moiseslopez <moiseslopeziglesias@gmail.com>
2025-10-18 20:02:44 +0200

    ex2Done

--- src/exercises/exercise-02.js
+++ src/exercises/exercise-02.js
@@ -5,7 +5,12 @@ Muestra el siguiente mensaje dependiendo de si un número es par o impar:
 Nota: even = par | odd = impar
 */
 export function exercise02(number) {
-  // Escribe tu solución aquí
-
+  let result;
+  if (number % 2 === 0) {
+    result = 'even';
+  }
+  else {
+    result = 'odd'
+  }
   return `Number ${number} is ${result}`;
 }
