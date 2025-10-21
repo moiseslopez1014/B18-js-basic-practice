5436acaed4953d550659a2e8d7e5f508011ba5de
moiseslopez <moiseslopeziglesias@gmail.com>
2025-10-21 23:05:22 +0200

    ex10Done

--- src/exercises/exercise-03.js
+++ src/exercises/exercise-03.js
@@ -7,7 +7,7 @@ Nota: vowels = "aeiou" (no incluyas acentos)
 export function exercise03(string) {
   const vowels = "aeiou";
   const splittered = string.split(""); // tranform all character in string into array
-  const filtrado = splittered.filter(letter => vowels.includes(letter));  // Set a new array filterin (if letter is a vowel)
-  const result = filtrado.length; // result is number of elements in filtered array
+  const filtered = splittered.filter(letter => vowels.includes(letter));  // Set a new array filterin (if letter is a vowel)
+  const result = filtered.length; // result is number of elements in filtered array
   return `Number of vowels in '${string}' is ${result}`;
 }
