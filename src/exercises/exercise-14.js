32756a5eae6e0dd4dc32ec1e006502710295e559
moiseslopez <moiseslopeziglesias@gmail.com>
2025-10-23 22:37:31 +0200

    ex14Done

--- src/exercises/exercise-14.js
+++ src/exercises/exercise-14.js
@@ -5,5 +5,9 @@ como parámetros filtra las películas que pertenezcan a la categoria "Drama"
 e incluye solo el título y la descripción.
 */
 export function exercise14(movies, categories) {
-  // Escribe tu solución aquí
+  
+  const ex14Movies = [...movies]; //makes a copy, as always
+  const ex14DramaCategory = categories.drama; // defines the category as a string
+  const filteredByDrama = ex14Movies.filter(movie => movie.category === ex14DramaCategory); // filter movies comparing the category string in categories with the string in movie - category
+  return filteredByDrama.map((movie) => `${movie.title}: ${movie.description}`); // Return the filtered array with title: and description
 }
