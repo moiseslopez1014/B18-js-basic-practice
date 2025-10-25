3008b1783549d395dd6d2fe4c9b0ebb523be5552
moiseslopez <moiseslopeziglesias@gmail.com>
2025-10-25 13:40:14 +0200

    ex17Done

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ showResult(13, exercises.exercise13(movies.slice(0, 5)));
 showResult(14, exercises.exercise14(movies.slice(0, 5), categories));
 showResult(15, exercises.exercise15(movies.slice(0, 5)));
 showResult(16, exercises.exercise16(movies));
-// showResult(17, exercises.exercise17("This is a hashtag"));
+showResult(17, exercises.exercise17("This is a hashtag"));
 // showResult(18, exercises.exercise18("Hello world"));
 // showResult(19, exercises.exercise19("Code deco"));
 // showResult(20, exercises.exercise20("10"));
